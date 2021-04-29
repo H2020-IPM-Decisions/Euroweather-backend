b9ed936691fea3191c148bd1ea3ccd8390023193
Tor-Einar Skog <tor-einar.skog@nibio.no>
2021-04-29 14:10:40 +0200

    Adding usage info

--- app/app.py
+++ app/app.py
@@ -22,32 +22,24 @@ import os
 from controller import Controller
 from custom_errors import NoDataAvailableError
 
-#os.environ["FIMEX_CHUNK_CACHE_SIZE"] = "218523238400" # TEST: See if this affects performance
 app = Flask(__name__)
 controller = Controller()
 sem = threading.Semaphore()
 
 @app.route('/')
 def get_forecasts():
-    #sem.acquire()
-    #print("Thread_ident: %s" % str(threading.get_ident()));
-    #print("get_forecasts() called")
+
     longitude = request.args.get("longitude", None) # WGS84
     latitude = request.args.get("latitude", None) # WGS84
     parameters = request.args.get("parameters", None) # Comma separated list
     if longitude == None or latitude == None:
-        return "BAD REQUEST: Missing longitude and/or latitude", 403
+        return render_template("usage.html")
     try:
         parameters = None if parameters == None else [int(i) for i in parameters.split(",")]
     except ValueError as e:
         return "BAD REQUEST: Error in specified weather parameters: %s" % e, 403
-    
-    #controller = Controller()
     try:
         data = controller.get_weather_data(longitude, latitude, parameters)
-        #sem.release()
-    
         return data
     except NoDataAvailableError as e:
         return "SERVICE UNAVAILABLE: Unfortunately, there is no data available at the moment.", 503
-    #print(parameters)
