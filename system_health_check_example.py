8befa7a5d9b664f172e54f602d9854d77e165c00
Tor-Einar Skog <tor-einar.skog@nibio.no>
2024-04-18 15:18:55 +0200

    Add file contents check

--- system_health_check_example.py
+++ system_health_check_example.py
@@ -24,6 +24,7 @@ import os
 import sys
 import glob
 import psutil
+import netCDF4 as nc
 from datetime import datetime, timezone, timedelta
 
 from smtplib import SMTP
@@ -43,22 +44,28 @@ DEBUG = False
 error_msg = []
 exit_code = 0
 
-# Assuming timestamps in file names are on form YYYYMMDD[HH] <= HH is optional
-# This makes alphanumeric comparison work
-def check_recent_file(file_expr, timestamp_expr, hours_delta):
-    global error_msg, exit_code
-    # Check how recent the .nc-files in perl/outdir are
+############# Method definitions #################
+
+def get_most_recent_file(file_expr):
     glob_expr = file_expr % "*"
     nc_files = glob.glob(f"{SITE_ROOT}{DATA_DIR_RELATIVE_PATH}{glob_expr}")
     most_recent_file = ""
     for nc_file in nc_files:
         nc_file = os.path.basename(nc_file) 
         most_recent_file = nc_file if nc_file > most_recent_file else most_recent_file
+    return f"{SITE_ROOT}{DATA_DIR_RELATIVE_PATH}{most_recent_file}"
 
+# Assuming timestamps in file names are on form YYYYMMDD[HH] <= HH is optional
+# This makes alphanumeric comparison work
+def check_recent_file(file_expr, timestamp_expr, hours_delta):
+    global error_msg, exit_code
+    # Check how recent the .nc-files in python/outdir are
+    most_recent_file = get_most_recent_file(file_expr)
     # Get the date stamp for now - 12 h, create a "allYYYYmmddhh.nc" filename and compare
     threshold_filename=f"{file_expr}" % datetime.strftime(datetime.now(timezone.utc) + timedelta(hours=hours_delta),timestamp_expr)
 
     if DEBUG:
+        glob_expr = file_expr % "*"
         print(f"{SITE_ROOT}{DATA_DIR_RELATIVE_PATH}{glob_expr}")
         print("%s %s" %(most_recent_file,threshold_filename))
     
@@ -66,13 +73,36 @@ def check_recent_file(file_expr, timestamp_expr, hours_delta):
         exit_code = 1
         error_msg.append(f"The latest NetCDF file {most_recent_file} is out of date")
 
+def check_most_recent_file_contents(file_expr, expected_time_steps, expected_params):
+    global error_msg, exit_code
+    most_recent_file = get_most_recent_file(file_expr)
+    dataset_metadata = nc.Dataset(f"{most_recent_file}", "r")
+    timesteps = dataset_metadata.variables["time"][:]
+    if len(timesteps) != expected_time_steps:
+        exit_code = 1
+        error_msg.append(f"The latest NetCDF file {most_recent_file} contains {len(timesteps)} timesteps, but we expected {expected_time_steps}")
+    param_names = dataset_metadata.variables.keys()
+    for expected_param in expected_params:
+        if expected_param not in param_names:
+            exit_code = 1
+            error_msg.append(f"Parameter {expected_param} missing in {most_recent_file}")
+
+############### System checks ####################
 
 # The latest allYYYYMMDDHH.nc should not be older than 12 hours
 check_recent_file("all%s.nc", "%Y%m%d%H", -12)
+# The latest allYYYYMMDDHH.nc should contain 79 timesteps and the parameters listed
+check_most_recent_file_contents("all%s.nc", 79,['relative_humidity_2m', 'total_precipitation', 'air_temperature_2m', 'x_wind_10m', 'y_wind_10m', 'hourly_precipitation', 'wind_speed_10m'])
+
 # The latest daily_archive_YYYYMMDD.nc should not be older than from tomorrow
 check_recent_file("daily_archive_%s.nc", "%Y%m%d", 24)
+# The latest daily_archive_YYYYMMDD.nc should contain 24 timesteps and the parameters listed
+check_most_recent_file_contents("daily_archive_%s.nc", 24,['relative_humidity_2m', 'air_temperature_2m', 'hourly_precipitation', 'wind_speed_10m'])
+
 # The latest daily_accumulated_YYYYMMDD.nc should not be older than from tomorrow
 check_recent_file("daily_accumulated_%s.nc", "%Y%m%d", 24)
+# The latest daily_accumulated_YYYYMMDD.nc should contain one timestemp and the parameters listed
+check_most_recent_file_contents("daily_accumulated_%s.nc", 1,['air_temperature_2m_max', 'air_temperature_2m_min', 'air_temperature_2m_mean', 'relative_humidity_2m_mean', 'relative_humidity_2m_max', 'total_precipitation', 'mean_wind_speed_10m'])
 
 # When only DISK_LIMIT_GB left on disk, alert
 DISK_LIMIT_GB = 400 # 400 GB is a little over a month's worth of files
@@ -81,6 +111,12 @@ free_space = round(psutil.disk_usage(".").free / 1073741824, 1) # 1073741824 byt
 if(free_space < DISK_LIMIT_GB):
     exit_code = 1
     error_msg.append(f"Warning: There is only {free_space} GB left on the data disk.")
+
+if(DEBUG):
+    print("\n".join(error_msg))
+
+
+################# Alerts/notifications ################
 
 if exit_code != 0 and SEND_EMAIL_ALERT:
     if DEBUG:
